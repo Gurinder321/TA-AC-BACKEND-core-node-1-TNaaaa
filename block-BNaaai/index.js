8cd0940a5155de6af3c27d5b2ec1d9e34e3cf0dc
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-03-18 18:39:28 -0700

    Not Done

---
+++ block-BNaaai/index.js
@@ -0,0 +1,7 @@
+// create file index.js
+var url = require('url');
+const parsedUrl = url.parse(
+  'https://blog.altcampus.io:80/students/register?name=altcampus&gender=male',
+  true
+);
+console.log(parsedUrl);
